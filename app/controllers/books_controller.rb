dbf25a497c05b311415b0b467ab7c880fcddd3df
YOUSUKE0601 <tyousuke0601>
2021-03-01 12:05:40 +0000

    [Update]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,21 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
   end
 
   def index
-    @books = Book.all
+     @book = Book.new
+     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render 'index'
@@ -34,16 +38,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
